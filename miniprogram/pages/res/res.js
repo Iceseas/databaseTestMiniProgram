c270040c080505fe9aa05bda8c0f0dad5cf0ed1a
SeA <Iceseas@users.noreply.github.com>
2021-01-26 16:41:37 +0800

    1.26

--- miniprogram/pages/res/res.js
+++ miniprogram/pages/res/res.js
@@ -49,17 +49,6 @@ Page({
             isloginres: app.globalData.islogin
         })
     },
-    onShow: function() {
-    },
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    },
-    onPageScroll: function(e) {
-
-    },
     swichisloginres() {
         app.globalData.islogin = !app.globalData.islogin
         if (app.globalData.islogin) {
